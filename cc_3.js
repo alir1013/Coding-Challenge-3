d4295538e63aac4058620f6b46f9cbafe38f1263
Roan Ali <alir13@usf.edu>
2024-09-08 14:35:05 -0400

    Implement calculateAverageSales function

--- cc_3.js
+++ cc_3.js
@@ -1,6 +1,7 @@
 //Task 1:Creating a function to calculate average sales 
 function calculateAverageSales (sales){
-    const totalsales= sales.reduce[1,5,9,4,7];
+    const totalsales= sales.reduce((total,sale)=> total+sale);
+    totalsales=sales
     return totalsales/sales.length;
 }
-console.log(`Average sales:,{calculateAverageSales}`);
+console.log(calculateAverageSales(sales));
