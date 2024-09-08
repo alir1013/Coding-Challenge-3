288886f5db734d6b1123758ae320fec498de0507
Roan Ali <alir13@usf.edu>
2024-09-08 14:57:55 -0400

    Create determinePerformanceRating function

--- cc_3.js
+++ cc_3.js
@@ -7,3 +7,14 @@ function calculateAverageSales (sales){
 const sales=[100,563,901,423,712] //Sales Data 
 console.log("Average Sales:",calculateAverageSales(sales)); //Output:539.8
 
+//Task 2: Create a function to determine performance rating
+function determinePerformanceRating (averageSales){
+    if (averageSales>10000)
+        return "Excellent";
+    else if (averageSales >= 7000 && averageSales <= 10000)
+        return "Good";
+    else if (averageSales >= 4000 && averageSales <= 7000)
+        return "Satisfactory";
+    if(averageSales< 4000)
+        return "Needs Improvement";
+}
