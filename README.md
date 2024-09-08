f761288b4cbb2f13630c9d121f7f00ffde7e0b49
Roan Ali <alir13@usf.edu>
2024-09-08 13:54:48 -0400

    Implement calculateAverageSales function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Coding-Challenge-3
+//Gain insights into the team's performance for bonuses and targeted training
---
+++ cc_3.js
@@ -0,0 +1,6 @@
+//Task 1:Creating a function to calculate average sales 
+function calculateAverageSales (sales){
+    const totalsales= sales.reduce[1,5,9,4,7];
+    return totalsales/sales.length;
+}
+console.log("Average sales");
