a26b900690779224fd3c625684eaea2f63e0c39a
Roan Ali <alir13@usf.edu>
2024-09-08 19:20:21 -0400

    Develop findTopAndBottomPerformers Function

--- cc_3.js
+++ cc_3.js
@@ -23,8 +23,8 @@ console.log("Performance Rating:",determinePerformanceRating(averageSales)); //O
 
 //Task 3:Creating a function to identify top and bottom performers
 function findTopAndBottomPerformers(salesPerson)
-    const maxSales=Math.max(...salesPerson.map(people=>people.totalSales));
-    const minSales= Math.min(...salesPerson.map(people=>people.totalSales));
+    maxSales=Math.max(...salesPerson.map(people=>people.totalSales));
+    minSales= Math.min(...salesPerson.map(people=>people.totalSales));
 
     const {topPerformer,bottomPerformer}= salesPerson.reduce;
     const salesPerson= [
