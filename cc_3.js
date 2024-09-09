c4459148705a2ffe947160e6fd973b9dce8d22ba
Roan Ali <alir13@usf.edu>
2024-09-08 21:02:48 -0400

    Test functions with sample sales data

--- cc_3.js
+++ cc_3.js
@@ -62,4 +62,12 @@ function generatePerformanceReport (salesData) {
         bottomPerformer,
     };
     };
-console.log ("Performace Report:",PerformanceReports);
+console.log ("Performace Report:",PerformanceReports); //Output:
+
+//Task 5: Testing the Functions with Sample Data
+const salesData=[
+    {name: 'Alice',sales:[12000,15000,13000]},
+    {name: 'Bob', sales[7000,6000,7500]},
+    {name: 'Charlie',sales[3000,4000,3500]},
+    {name: `Diana`,sales[9000,8500,9200]},
+];
