1d11f74c1e8215c9970235d188f31290d75206b0
Roan Ali <alir13@usf.edu>
2024-09-08 21:05:48 -0400

    Implement generatePerformanceReport function

--- cc_3.js
+++ cc_3.js
@@ -68,8 +68,8 @@ console.log ("Performace Report:",PerformanceReport); //Output:
 //Task 5: Testing the Functions with Sample Data
 const salesData=[
     {name: 'Alice',sales:[12000,15000,13000]},
-    {name: 'Bob', sales[7000,6000,7500]},
-    {name: 'Charlie',sales[3000,4000,3500]},
-    {name: `Diana`,sales[9000,8500,9200]},
+    {name: 'Bob', sales:[7000,6000,7500]},
+    {name: 'Charlie',sales:[3000,4000,3500]},
+    {name: `Diana`,sales:[9000,8500,9200]},
 ];
 console.log 
